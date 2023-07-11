79d40fe696ea223a2193f0e260ccb480a4a41f25
Guido Cerqueira <euguidocerqueira@gmail.com>
2023-07-11 17:51:05 -0300

    initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,5 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -15,8 +12,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="pt-br">
+      <body className="bg-gray-800 text-white">{children}</body>
     </html>
   )
 }
